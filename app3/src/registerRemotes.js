e3b26ffa53a4f050744dbc07ec67525c703674dd
Eszter Pálmai <eszter.palmai@shoprenter.hu>
2025-01-21 16:55:19 +0100

    add even more apps

--- app3/src/registerRemotes.js
+++ app3/src/registerRemotes.js
@@ -4,13 +4,10 @@ import remoteDataSource from "./../remotes.json"
 
 export async function registerRemotess() {
     // 1. __webpack_init_sharing__
-    await require("module-federation-runtime").initSharing("default")
+    await initSharing("default")
 
-// 2. __webpack_share_scopes__
-    require("module-federation-runtime").shareScopes
-
-// 3. dynamic-remotes
-    const register = await require("module-federation-runtime").registerRemotes(
+    // 3. dynamic-remotes
+    await require("module-federation-runtime").registerRemotes(
         remoteDataSource
     )
 
